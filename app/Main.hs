dbfcd79c2049c77c6302a7d7924c8b2c1ae65fab
craigdallimore <decoy9697@gmail.com>
2019-01-29 22:01:55 +1300

    Parsing all entries

--- app/Main.hs
+++ app/Main.hs
@@ -1,69 +1,81 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
 
 module Main where
 
 import Prelude
 import qualified Data.ByteString.Char8 as B
+import Control.Applicative ((<|>))
 import Text.Trifecta
-import Text.Trifecta.Combinators (lookAhead)
+import Text.RawString.QQ
 
 fp :: FilePath
 fp = "/home/decoy/Documents/vimwiki/study/category-theory/terminology.md"
 
-type Modeline = String
-type H1 = String
-type H2 = String
+newtype Modeline = Modeline String deriving (Show)
+newtype H1 = H1 String deriving (Show)
+newtype H2 = H2 String deriving (Show)
+newtype Line = Line String deriving (Show)
 data Entry = Entry H2 String
 data Wiki = Wiki Modeline H1 [Entry]
 
+data LineOrH2 = IsLine Line | IsH2 H2
+
 instance Show Entry where
-  show (Entry h2 lines) = "## " <> h2 <> "\n" <> lines
+  show (Entry (H2 h2) lines) = "## " <> h2 <> "\n" <> lines
 
 instance Show Wiki where
-  show (Wiki modeline h1 entries) = all where
+  show (Wiki (Modeline modeline) (H1 h1) entries) = all where
     all = modeline <> "\n\n" <>
           "# " <> h1 <> "\n\n" <>
           foldr (\e acc -> show e <> acc) mempty entries
-          <> show (length entries)
+          <> "Number of entries:" <> show (length entries)
+
+--------------------------------------------------------------------------------
 
 isNewLine :: Char -> Bool
 isNewLine = (==) '\n'
 
---------------------------------------------------------------------------------
+skipNewlines :: Parser ()
+skipNewlines = skipMany newline
 
-skipSpace :: Parser ()
-skipSpace = skipMany (oneOf "\n ")
+tilEol :: Parser String
+tilEol = many (notChar '\n')
 
 parseModeline :: Parser Modeline
 parseModeline = do
-  x <- manyTill anyChar (string "\n")
-  return x
+  x <- tilEol
+  return (Modeline x)
 
 parseH1 :: Parser H1
-parseH1 = string "# " >> manyTill anyChar newline
+parseH1 = do
+  string "# "
+  h1 <- tilEol
+  return (H1 h1)
 
 parseH2 :: Parser H2
-parseH2 = string "## " >> manyTill anyChar newline
+parseH2 = do
+  string "## "
+  h2 <- tilEol
+  return (H2 h2)
 
-parseLines :: Parser String
-parseLines = manyTill anyChar (lookAhead parseH2)
+parseEntry :: Parser Entry
+parseEntry = do
+  h2 <- parseH2
+  newline
+  lines <- many (notFollowedBy parseH2 >> anyChar)
+  return $ Entry h2 lines
 
-newtype H2orOther = H2 | Other
-
-parseEntries :: Parser [Entry]
-parseEntries = do
-  content <- (H2 <$> try parseH2) <|> (Other <$> manyTill anyChar newline)
-
-  Entry <$> parseH2 <*> parseLines
+-- Think, what do we want?
+-- We want to capture `many anyChar` lines,
+-- but stop caturing after arriving at an h2 line
 
 parseWiki :: Parser Wiki
 parseWiki = do
   modeline <- parseModeline
-  skipSpace
+  skipNewlines
   h1 <- parseH1
-  skipSpace
-  -- entries <- many parseEntry
-  entries <- parseEntries
+  entries <- many (skipNewlines >> parseEntry)
   return (Wiki modeline h1 entries)
 
 --------------------------------------------------------------------------------
